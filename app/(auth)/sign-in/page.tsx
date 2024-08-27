de79ffe6727501182ea7e27e7638685b22d1eed8
Trechno <sainyambothra7@gmail.com>
2024-08-27 23:53:16 +0530

    major completion

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,112 @@
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
+import axios, { AxiosError } from 'axios'
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+import { ApiResponse } from "@/types/ApiResponse"
+import { Form, 
+  FormControl,  
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
 
 
-const Signin = () => {
+const page = () => {
+
+const { toast } = useToast()
+const router = useRouter()
+
+// zod implementation
+const form = useForm<z.infer<typeof signInSchema>>({
+  resolver : zodResolver(signInSchema),
+  defaultValues: {
+    identifier: '',
+    password: '',
+  }
+})
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+     const result = await signIn('credentials', {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password
+      })
+      if(result?.error) {
+        toast({
+          title: "Login Failed",
+          description: "Incorrect username or password",
+          variant: "destructive"
+        })
+      } 
+      if (result?.url) {
+        router.replace('/dashboard')
+      }
+    }
+
   return (
-    <div>Signin</div>
+    
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+          Welcome 
+        </h1>
+        <p className="mb-4">Sign in to continue your conversations</p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="identifier"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email/Username</FormLabel>
+              <FormControl>
+                <Input placeholder="email/username" {...field}/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="password" {...field}/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          SignIn
+        </Button>
+        </form>
+      </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Signin
+export default page
---
+++ app/u/[username]/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>message</div>
+  )
+}
+
+export default page
